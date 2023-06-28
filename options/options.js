4540dd65b37b710ff4ce75078b254480976c2426
Crowdbotics <engineering@crowdbotics.com>
2023-06-28 10:47:37 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "broken-disk-dev-95437",
+  url: "https://broken-disk-dev-95437.botics.co",
+  api: "https://broken-disk-dev-95437.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
